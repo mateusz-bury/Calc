478eb1693fd6ed317943c9e96c9fe161bf7c7d1d
mateuszbury420 <mateuszbury875@gmail.com>
2023-10-02 20:41:51 +0200

    Add project files.

---
+++ Controllers/CalcController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CalcController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new calc());
+        }
+
+        [HttpPost]
+
+        public ActionResult Index(calc c, string calculate)
+        {
+
+            if (calculate == "add")
+            {
+                c.tot = c.no1 + c.no2;
+            }
+            else if (calculate == "min")
+            {
+                c.tot = c.no1 - c.no2;
+            }
+            else if (calculate == "sub")
+            {
+                c.tot = c.no1 * c.no2;
+            }
+            else
+            {
+                c.tot = c.no1 / c.no2;
+            }
+            return View(c);
+        }
+    }
+}
---
+++ Models/calc.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Models
+{
+    public class calc
+    {
+        public int no1 { get; set; }
+
+        public int no2 { get; set; }
+
+        public int tot { get; set; }
+
+    }
+}
